62fc3a4fea5b6706bc4937a5d419d643e73602b4
MaiTran2003 <tranthengocmaia14@gmail.com>
2024-07-10 16:39:34 +0700

    add file bai 1

---
+++ Bai1.cpp
@@ -0,0 +1,18 @@
+void main()
+{
+int a = 0, b = 0;
+printf("Nhap a va b: ");
+scanf("%d%d", &a, &b);
+try
+{
+if(b == 0)
+throw "So chia khong duoc la 0";
+else
+int t = a/b;
+}
+catch(const char *st)
+{
+printf("Loi: %s", st);
+}
+getch();
+}
